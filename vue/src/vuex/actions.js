ff587020a70b53b208a59e1ad6fc62ce68b39c6e
Jefferson Daniel <jeffersondanielss@gmail.com>
2017-01-15 16:42:29 -0200

    update repo

--- vue/src/vuex/actions.js
+++
@@ -1,4 +0,0 @@
-export const getUser = ({dispatch}, username) => dispatch('GET_USER', username)
-export const getRepo = ({dispatch}, username) => dispatch('GET_REPO', username)
-export const getOrg = ({dispatch}, username) => dispatch('GET_ORG', username)
-export const setUser = ({dispatch}, username) => dispatch('SET_USER', username)
